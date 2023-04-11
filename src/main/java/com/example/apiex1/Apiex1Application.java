e9e5fa12f9ea7c410993d28b9195839b92628555
Ramilade <rami0904@stud.kea.dk>
2023-04-11 21:46:05 +0200

    First commit

---
+++ src/main/java/com/example/apiex1/Apiex1Application.java
@@ -0,0 +1,13 @@
+package com.example.apiex1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Apiex1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Apiex1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/apiex1/Apiex1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apiex1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Apiex1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
